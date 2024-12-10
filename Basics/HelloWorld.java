7625b70086e452097e07fa6f87d0e097c8c2f99d
Abrar Ateeq <akabrar5@gmail.com>
2024-12-10 09:04:55 +0530

    HelloWorld.java

--- Basics/HelloWorld.java
+++ Basics/HelloWorld.java
@@ -1,4 +1,4 @@
-public class firstProgram {
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is my First Java Program");

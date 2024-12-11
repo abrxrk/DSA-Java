4fff5cde0a85f4615fdf2b8a9d3e248b4b581984
Abrar Ateeq <akabrar5@gmail.com>
2024-12-11 13:17:09 +0530

    Loops.java

--- Basics/Loops.java
+++ Basics/Loops.java
@@ -9,27 +9,27 @@ public class Loops {
           }
          */
         // Q: Print numbers from 1 to 5
-//        for (int i = 1; i <6 ; i++) {
-//            System.out.println(i);
-//        }
+        for (int i = 1; i <6 ; i++) {
+            System.out.println(i);
+        }
         // print numbers from 1 to n
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Enter the number- ");
-//        int n = input.nextInt();
-//        for (int i = 1; i <=n ; i++) {
-//            System.out.println(i);
-//        }
+        Scanner input = new Scanner(System.in);
+       System.out.print("Enter the number- ");
+       int n = input.nextInt();
+       for (int i = 1; i <=n ; i++) {
+           System.out.println(i);
+       }
         /*
         Syntax of while loop:
         while (condition) {
         // body
         }
          */
-//        int num = 1;
-//        while (num<=5){
-//            System.out.println(num);
-//            num++;
-//        }
+       int num = 1;
+       while (num<=5){
+            System.out.println(num);
+            num++;
+       }
         // do while loop runs atleast once and then checks the condition
     }
 }

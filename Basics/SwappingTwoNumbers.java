c79d9648b6a41596fe0ca75690671edc9deec544
Abrar Ateeq <akabrar5@gmail.com>
2024-12-14 12:02:40 +0530

    Add files via upload

---
+++ Basics/SwappingTwoNumbers.java
@@ -0,0 +1,11 @@
+public class SwappingTwoNumbers {
+    public static void main(String[] args) {
+        int a = 5;
+        int temp = a;
+        int b = 6;
+        a = b;
+        b = temp;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
